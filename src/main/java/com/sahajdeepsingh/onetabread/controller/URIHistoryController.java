37b4de4126c20042108611312bcb070e2afcb2cf
Sahajdeep Singh <impulseVD@hotmail.com>
2025-06-03 16:32:44 -0400

    Commented rough idea in last two controller classes, added a post method in one of them.

--- src/main/java/com/sahajdeepsingh/onetabread/controller/URIHistoryController.java
+++ src/main/java/com/sahajdeepsingh/onetabread/controller/URIHistoryController.java
@@ -1,4 +1,44 @@
 package com.sahajdeepsingh.onetabread.controller;
 
+import com.sahajdeepsingh.onetabread.model.URIHistory;
+import com.sahajdeepsingh.onetabread.service.URIHistoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend to access
+@RestController
+@RequestMapping("/users/{user_id}/books/{book_id}/urihistory")
 public class URIHistoryController {
+    // So we need a post and get method, maybe a delete method too, also update
+    // update is for when a url is visited and its already posted, this can change
+    // the visited_at so it shows up in front and there is no duplicate
+    // post is when a url is newly visited, only if it followed the pattern
+    // get is to get the latest 5 url that goes to same book_id
+    // delete is for probably deleting a url if not within latest 5 so get is easier
+    // delete can ammortize the cost of get
+
+    private final URIHistoryService uriHistoryService;
+
+    public URIHistoryController(URIHistoryService uriHistoryService) {
+        this.uriHistoryService = uriHistoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createURIHistory(@PathVariable Long user_id,
+                                                 @PathVariable Long book_id,
+                                                 @RequestBody URIHistory uriHistory,
+                                                 UriComponentsBuilder ucb) {
+        URIHistory newURIHistory = uriHistoryService.save(uriHistory);
+        if (uriHistory.getId() != null) {
+            URI locationOfNewURIHistory = ucb
+                    .path("/users/{user_id}/books/{book_id}/urihistory/{id}")
+                    .buildAndExpand(user_id, book_id, newURIHistory.getId())
+                    .toUri();
+            return ResponseEntity.created(locationOfNewURIHistory).build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
 }
--- src/main/java/com/sahajdeepsingh/onetabread/controller/URIPatternController.java
+++ src/main/java/com/sahajdeepsingh/onetabread/controller/URIPatternController.java
@@ -1,4 +1,21 @@
 package com.sahajdeepsingh.onetabread.controller;
 
+import com.sahajdeepsingh.onetabread.service.URIPatternService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend to access
+@RestController
 public class URIPatternController {
+    // all 4 types needed
+    // post is for posting a new pattern for a book
+    // update is for changing the pattern of the book, history will bee kept
+    // get is for comparing every url with it to know if history needs to record
+    // delete is probably when book gets deleted
+
+    private final URIPatternService uriPatternService;
+
+    public URIPatternController(URIPatternService uriPatternService) {
+        this.uriPatternService = uriPatternService;
+    }
 }
